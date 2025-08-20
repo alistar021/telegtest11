38090899f15f5f14fe6d20bc6f12f6d797dad2f1
alistar021 <129028285+alistar021@users.noreply.github.com>
2025-08-20 04:45:20 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,19 @@
-from telegram import Update, Bot
+from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-BOT_TOKEN = "8476998300:AAEcUHxNBmBdoYvm3Q3DV9kftBho-ABzJRE"
+BOT_TOKEN = "توکن_ربات_تو"
 CHANNEL_USERNAME = "@alialisend123"
 
-# ====== دستور استارت ======
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! پیام شما به کانال ارسال می‌شود.")
 
-# ====== دریافت پیام و ارسال به کانال ======
 async def forward_to_channel(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_text = update.message.text
     bot = context.bot
     await bot.send_message(chat_id=CHANNEL_USERNAME, text=message_text)
     await update.message.reply_text("پیام شما ارسال شد ✅")
 
-# ====== اجرای ربات ======
 app = ApplicationBuilder().token(BOT_TOKEN).build()
-
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), forward_to_channel))
 
